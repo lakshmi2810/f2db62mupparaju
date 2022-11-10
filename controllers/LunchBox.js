be1d2fb3ecedc03c26942181f625c32fb6359651
lakshmi2810 <35185775+lakshmi2810@users.noreply.github.com>
2022-11-09 19:50:40 -0600

    add all the files

--- controllers/LunchBox.js
+++ controllers/LunchBox.js
@@ -1,21 +1,49 @@
 var LunchBox = require('../models/LunchBox');
-// List of all Costumes
-exports.LunchBox_list = function(req, res) {
- res.send('NOT IMPLEMENTED: LunchBox list');
+// List of all LunchBoxs
+exports.LunchBox_list = async function (req, res) {
+    try {
+        theLunchBoxs = await LunchBox.find();
+        res.send(theLunchBoxs);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
-// for a specific Costume.
-exports.LunchBox_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: LunchBox detail: ' + req.params.id);
+/*
+// List of all LunchBoxs
+exports.LunchBox_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: LunchBox list');
+};*/
+// for a specific LunchBox.
+exports.LunchBox_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: LunchBox detail: ' + req.params.id);
 };
-// Handle Costume create on POST.
-exports.LunchBox_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: LunchBox create POST');
+// Handle LunchBox create on POST.
+exports.LunchBox_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new LunchBox();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"LunchBox_name":"phoenix","LunchBox_rent":10000,"LunchBox_size":1000}
+    document.LunchBox_name = req.body.LunchBox_name;
+    document.LunchBox_price = req.body.LunchBox_price;
+    document.LunchBox_size = req.body.LunchBox_size;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
-// Handle Costume delete form on DELETE.
-exports.LunchBox_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: LunchBox delete DELETE ' + req.params.id);
+// Handle LunchBox delete form on DELETE.
+exports.LunchBox_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: LunchBox delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
-exports.LunchBox_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: LunchBox update PUT' + req.params.id);
+// Handle LunchBox update form on PUT.
+exports.LunchBox_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: LunchBox update PUT' + req.params.id);
 };
--- controllers/api.js
+++ controllers/api.js
@@ -1,7 +1,7 @@
 // API for our resources
 exports.api = function(req, res) {
     res.write('[');
-    res.write('{"resource":"LunchBoxs", ');
+    res.write('{"resource":"LunchBox", ');
     res.write(' "verbs":["GET","PUT", "DELETE"] ');
     res.write('}');
     res.write(']')
