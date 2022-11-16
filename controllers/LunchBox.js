34f126ba9abe9dcb3b9d8a2ebbf3749d7e196e50
lakshmi2810 <35185775+lakshmi2810@users.noreply.github.com>
2022-11-16 17:24:54 -0600

    add all the files

--- controllers/LunchBox.js
+++ controllers/LunchBox.js
@@ -107,3 +107,46 @@ exports.LunchBox_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+   // Handle building the view for creating a LunchBox.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.LunchBox_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('LunchBoxcreate', { title: 'LunchBox Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+
+// Handle building the view for updating a LunchBox.
+// query provides the id
+exports.LunchBox_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await LunchBox.findById(req.query.id)
+    res.render('LunchBoxupdate', { title: 'LunchBox Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+
+   // Handle a delete one view with id from query
+exports.LunchBox_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await LunchBox.findById(req.query.id)
+    res.render('LunchBoxdelete', { title: 'LunchBox Delete', toShow:
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/LunchBox.js
+++ routes/LunchBox.js
@@ -4,4 +4,16 @@ var router = express.Router();
 
 /* GET LunchBoxs. */
 router.get('/', LunchBox_controlers.LunchBox_view_all_Page );
+
+
+/* GET detail LunchBox page */
+router.get('/detail', LunchBox_controlers.LunchBox_view_one_Page);
+/* GET create LunchBox page */
+router.get('/create', LunchBox_controlers.LunchBox_create_Page);
+
+/* GET create update page */
+router.get('/update', LunchBox_controlers.LunchBox_update_Page);
+
+/* GET delete LunchBox page */
+router.get('/delete', LunchBox_controlers.LunchBox_delete_Page);
 module.exports = router;
